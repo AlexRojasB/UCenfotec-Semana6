5376f9a380ab4f98c6f1e7c4e47cc0d382eb395f
Alexander Rojas <alexrrojas.b@gmail.com>
2021-09-18 10:19:06 -0600

    cocoapods

---
+++ lab10/lab10/lab10App.swift
@@ -0,0 +1,17 @@
+//
+//  lab10App.swift
+//  lab10
+//
+//  Created by Alexander Rojas Benavides on 9/18/21.
+//
+
+import SwiftUI
+
+@main
+struct lab10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
